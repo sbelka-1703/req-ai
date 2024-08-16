23546b16b0e462b230a0a9ce980700b723160596
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2024-08-16 14:57:03 -0700

    output of number of tubes and kind of tubes are acurate when all the pages are converted to text

--- app/api/req-pdf/route.ts
+++ app/api/req-pdf/route.ts
@@ -1,126 +1,300 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { fromBuffer } from 'pdf2pic'
 import { Buffer } from 'buffer'
+import { PDFDocument } from 'pdf-lib' // Import pdf-lib for page count
+
 import OpenAI from 'openai'
 import fs from 'fs'
 import path from 'path'
 const openAi = new OpenAI({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY // Ensure your API key is correctly set
 })
-const tests = [
+
+const totalTubeVolume = [
+  { tubeType: 'gel-barrier', totalVolume: 6 },
+  { tubeType: 'lavender-top', totalVolume: 4 }
+]
+
+const testsReference = [
   {
     cptCode: '86140',
     testName: 'C-Reactive Protein (CRP), Quantitative',
     tubeType: 'gel-barrier',
-    volume: 1, // Volume in mL
-    container: 'Red-top tube or gel-barrier tube'
+    volume: 1 // Volume in mL
   },
   {
     cptCode: '81001',
     testName: 'Urinalysis, Complete',
-    tubeType: 'gel-barrier',
-    volume: 10, // Volume in mL (typical)
-    container: 'Gel-barrier tube'
+    tubeType: 'Vacutainer® red/yellow urine transport tube',
+    volume: 10 // Volume in mL (typical)
   },
   {
     cptCode: '82550',
-    testName: 'Creatine Kinase (CK), Total',
+    testName: 'Creatine Kinase (CK), Total Serum',
     tubeType: 'gel-barrier',
-    volume: 1, // Volume in mL
-    container: 'Gel-barrier tube'
+    volume: 1 // Volume in mL
   },
   {
     cptCode: '82306',
     testName: 'Vitamin D, 25-Hydroxy',
     tubeType: 'gel-barrier',
-    volume: 1, // Volume in mL
-    container: 'Red-top tube or gel-barrier tube'
+    volume: 1 // Volume in mL
   },
   {
     cptCode: '86038',
     testName: 'ANA by IFA, Reflex to Titer and Pattern',
     tubeType: 'gel-barrier',
-    volume: 2, // Volume in mL
-    container: 'Red-top tube or gel-barrier tube'
+    volume: 2 // Volume in mL
   },
   {
     cptCode: '86235',
     testName: 'Antinuclear Antibodies (ANA)',
     tubeType: 'gel-barrier',
-    volume: 2, // Volume in mL
-    container: 'Red-top tube or gel-barrier tube'
+    volume: 2 // Volume in mL
   },
   {
     cptCode: '81374',
     testName: 'HLA B27 Disease Association',
     tubeType: 'lavender-top',
-    volume: 3, // Volume in mL
-    container: 'Lavender-top (EDTA) tube'
+    volume: 3 // Volume in mL
   },
   {
     cptCode: '86200',
     testName: 'Anti-CCP (Cyclic Citrullinated Peptide) Antibodies',
     tubeType: 'gel-barrier',
-    volume: 1, // Volume in mL
-    container: 'Red-top tube or gel-barrier tube'
+    volume: 1 // Volume in mL
   },
   {
     cptCode: '86635',
     testName: 'Coccidioides Abs, IgG/IgM',
     tubeType: 'gel-barrier',
-    volume: 1, // Volume in mL
-    container: 'Red-top tube or gel-barrier tube'
+    volume: 1 // Volume in mL
   },
   {
     cptCode: '82164',
     testName: 'Angiotensin-Converting Enzyme',
     tubeType: 'gel-barrier',
-    volume: 1, // Volume in mL
-    container: 'Gel-barrier tube'
+    volume: 1 // Volume in mL
   },
   {
     cptCode: '86160',
     testName: 'Complement C4, Serum',
     tubeType: 'gel-barrier',
-    volume: 1, // Volume in mL
-    container: 'Gel-barrier tube'
+    volume: 1 // Volume in mL
   },
   {
     cptCode: '84550',
     testName: 'Uric Acid, Serum',
     tubeType: 'gel-barrier',
-    volume: 1, // Volume in mL
-    container: 'Gel-barrier tube'
+    volume: 1 // Volume in mL
   },
   {
     cptCode: '86037',
     testName: 'ANCA Profile',
     tubeType: 'gel-barrier',
-    volume: 2, // Volume in mL
-    container: 'Gel-barrier tube'
+    volume: 2 // Volume in mL
   },
   {
     cptCode: '85025',
     testName: 'Complete Blood Count (CBC) With Differential',
     tubeType: 'lavender-top',
-    volume: 1, // Volume in mL
-    container: 'Lavender-top (EDTA) tube'
+    volume: 1 // Volume in mL
   },
   {
     cptCode: '80053',
     testName: 'Comprehensive Metabolic Panel',
     tubeType: 'gel-barrier',
-    volume: 1, // Volume in mL
-    container: 'Gel-barrier tube'
-  },
-  {
-    cptCode: '86480',
-    testName: 'QuantiFERON-TB Gold Plus',
-    tubeType: 'gel-barrier',
-    volume: 1, // Volume in mL
-    container: 'Gel-barrier tube'
+    volume: 1 // Volume in mL
   }
 ]
+
+// const documentText = `**LabCorp**
+
+// **Specimen/Provider Information**
+// Collection Date: 08/14/2024 @ 11:15
+// Ordering Provider: Rizwan, Mian
+
+// **Patient Information**
+// Patient Name:
+// Patient Id:
+// SSN:
+// Work Phone:
+
+// **Billing Information**
+// Primary Ins.: United HealthCare of all states
+// Address: PO Box 740800
+// City, St, Zip: Atlanta, GA 30374-0800
+// Workers' Comp: 906317801-0
+// Group#: 13352
+// Emp / Grp Name:
+
+// **Bill To: Third Party**
+// Secondary Ins. Code:
+// Secondary Ins. Address:
+// City, St. Zip:
+// Workers' Comp:
+// Group#:
+// Emp / Grp Name:
+// Insured:
+// Address:
+// City, St. Zip:
+// Relationship:
+
+// **Guarantee Information**
+// Name
+// Address
+// Sample Comment:
+
+// **Profiles / Tests**
+// | Order Code | CPT Code | Status   | Test Name                             | Transport Requirements    |
+// |------------|----------|----------|---------------------------------------|---------------------------|
+// | 006627     | 86140    | Routine  | C-Reactive Protein, Quant             | Room Temperature: Room Temperature |
+// | 011832     | 82525    | Routine  | Creative Kinase Total. Serum          | Room Temperature: Room Temperature |
+// | 061497     | 82306    | Routine  | Vitamin D, 25-Hydroxy                 | Room Temperature: Room Temperature |
+// | 161497     | 86003    | Routine  | Antinuclear Antibodies, IF            | Room Temperature: Room Temperature |
+// | 027492     | 86375    | Routine  | Signer's Ab, Anti-SSA/SS-B           | Room Temperature: Room Temperature |
+// | 060294     | 81747    | Routine  | HLA B 27 Disease Association          | Room Temperature: Room Temperature |
+// | 164065     | 86200    | Routine  | Rheumatoid Arthritis Profile          | Room Temperature: Room Temperature |
+
+// **Authorization**
+// Please sign and date
+// I hereby authorize the release of medical information related to the services described hereon and authorize payment of charges for laboratory services that are provided.
+
+// **Rizwan, Mian**
+// **08/14/2024**
+
+// **Patient Signature**
+// Date
+
+// [Link to app] https://app.kareo.com/labs-util/patient/179001234/labState/all
+
+// ---
+
+// Let me know if you need any further assistance!
+// Sure! Here’s the extracted text from the document:
+
+// ---
+
+// **LabCorp**
+
+// **Specimen/Provider Information**
+// Collection Date: 08/02/2024
+// Ordering Provider: Rizwan, M. 16:00
+// Lab Reference#: 28397071
+// UPIN / NPI: 1437380276
+
+// **Patient Information**
+// Patient Name:
+// Patient Id:
+// Date of Birth: 06/12/1973
+// Work Phone:
+
+// **Billing Information**
+// Primary Ins. Code: United HealthCare of all states
+// Address: PO Box 748000
+// City, St. Zip: Atlanta, GA 30374-0800
+// Worker's Comp: No
+// Group: 1945672-01
+// Emp / Grp Name:
+// Address:
+// City, St. Zip:
+// Relationship:
+
+// **Diagnosis Codes:** R53.82, R76.0, M25.59, M15.9
+
+// **Guarantor Information**
+// Name:
+// Address:
+
+// Sample Comment:
+
+// ---
+
+// **Profiles / Tests**
+// | Order Code | CPT Code | Status | Test Name | Transport Requirements |
+// |------------|----------|--------|-----------|-----------------------|
+// | 006267     | 87410    | Routine | C-Reactive Protein, Quant | Room Temperature: Room Temperature |
+// | 001362     | 82565    | Routine | Creative Kinase, Total Serum | Room Temperature: Room Temperature |
+// | 164796     | 8633...  | Routine | Coccidioides Abs, IgG/IgM, EIA | Room Temperature: Room Temperature |
+// | 164794     | 8636...  | Routine | Antinuclear Antibodies, IFA | Room Temperature: Room Temperature |
+// | 001700     | 86235... | Routine | Signer's Ab, Anti-SLA, A/S-B | Room Temperature: Room Temperature |
+// | 011016     | 82114    | Routine | Angiotensin-Converting Enzyme | Room Temperature: Room Temperature |
+// | 001384     | 86160    | Routine | Complement C4, Serum | Room Temperature: Room Temperature |
+// | 001950     | 86134    | Routine | Uric Acid, Serum | Room Temperature: Room Temperature |
+// | 164605     | 86200    | Routine | Rheumatoid Arthritis Profile | Room Temperature: Room Temperature |
+// | 163061     | 86037... | Routine | ANCA Profile | Room Temperature: Room Temperature |
+
+// **Authorization - Please sign and date**
+// I hereby authorize the release of medical information related to the services described herein and authorize payment of charges for laboratory services that are performed.
+
+// **Date:** 08/12/2024
+// **Patient Signature:**
+// **Date:**
+
+// ---
+
+// **https://app.kareo.com/labs-util/patient/17920224/labState/all**
+// Here's the extracted text from the document:
+
+// ---
+
+// **LabCorp**
+
+// Specimen/Provider Information
+// Collection Date: 08/13/2024 @ 12:45
+// Ordering Provider: Rizwan, Mian
+
+// **Patient Information**
+// Patient Name:
+// Patient Id:
+// SSN:
+// DOB: 07/26/1953
+// Work Phone:
+
+// **Billing Information**
+// Primary Ins.: Humana
+// Primary Ins. Address: P.O. Box 14601
+// City St. Zip: Lexington, KY 40512-4601
+// Workers Comp:
+// Group: H68606778
+
+// **Bill To: Third Party**
+// Secondary Ins.:
+// Secondary Ins. Address:
+// City St. Zip:
+// Workers Comp:
+// Policy:
+// Emp / Grp Name:
+// Insured:
+// Address:
+// City St. Zip:
+// Relationship:
+
+// **Guarantor Information**
+// Name:
+// Address:
+// Sample Comment:
+
+// **Profiles / Tests**
+// Order Code | CPT Code | Status | Test Name | Transport Requirements
+// ------------ | -------- | ------ | --------- | ----------------------
+// 005609 | 85325 | Routine | CBC with Differential/Platelet | Room Temperature
+// 006267 | 86140 | Routine | C-Reactive Protein, Quant | Room Temperature
+// 322000 | 80053 | Routine | Comp. Metabolic Panel (14) | Room Temperature
+// 164798 | 86635 | Routine | Coccidioides Abs, IgG/IgM, EIA | Room Temperature
+// 163061 | 86037 | Routine | ANCA Profile | Room Temperature
+
+// Authorization - Please sign and date
+// I hereby authorize the release of medical information related to the services described herein and authorize payment directly to LabCorp. I agree to assume responsibility for payment of charges for laboratory services that are not covered by my health insurance.
+
+// Rizwan, Mian 08/13/2024
+// Ordering Provider Electronic Signature: [Signature]
+// Patient Signature: [Signature]
+
+// [LINK]
+
+// ---
+// `
 
 export async function POST(request: NextRequest) {
   try {
@@ -134,23 +308,62 @@ export async function POST(request: NextRequest) {
     // Convert the file into a Buffer for pdf2pic
     const pdfBuffer = Buffer.from(await file.arrayBuffer())
 
+    // Get the number of pages in the PDF (using pdf-lib)
+    const pdfDoc = await PDFDocument.load(pdfBuffer)
+    const numberOfPages = pdfDoc.getPageCount()
+
     // Convert the PDF to images using pdf2pic
     const images = await convertPdfToImages(pdfBuffer)
 
-    const results = []
+    // Check if the number of images matches the number of pages
+    if (images.length !== numberOfPages) {
+      console.error('Mismatch in image count and number of pages in the PDF')
+      return NextResponse.json({ success: false, message: 'Mismatch in image count and number of pages in the PDF' })
+    }
 
-    // Send each image to OpenAI Vision API
-    for (const image of images) {
-      if (image) {
-        const visionResult = await pdfToText(image)
-        results.push(visionResult)
+    const results = []
+    const maxRetries = 3 // Set the maximum number of retries
+    const processedPages = []
+
+    // Send each image to OpenAI Vision API with retry mechanism
+    for (const [index, image] of images.entries()) {
+      let success = false
+      let retries = 0
+
+      while (!success && retries < maxRetries) {
+        try {
+          if (image) {
+            const visionResult = await pdfToText(image)
+            results.push(visionResult)
+            processedPages.push(index)
+            success = true // Mark success if the processing is successful
+          }
+        } catch (error) {
+          console.error(`Error processing image for page ${index + 1} (attempt ${retries + 1}):`, error)
+          retries++
+        }
+      }
+
+      if (!success) {
+        console.error(`Failed to process page ${index + 1} after ${maxRetries} retries.`)
       }
     }
 
-    const tubesNeeded = await textToTubes(results.join('\n'))
+    // Check if all pages were processed
+    if (processedPages.length < numberOfPages) {
+      console.error(`Processed ${processedPages.length} out of ${numberOfPages} pages.`)
+      return NextResponse.json({ success: false, message: 'Some pages were not processed successfully.' })
+    }
 
-    // Combine all text results and return as a single response
-    return NextResponse.json({ success: true, text: tubesNeeded })
+    const joinedResults = results.join('\n')
+    console.log('text from pdf', joinedResults)
+
+    const tubesAndVolumeNeeded = await textToTubeTypeAndVolume(joinedResults)
+    console.log('tubesAndVolumeNeeded', tubesAndVolumeNeeded)
+
+    const tubesNeeded = await totalTubesNeeded(tubesAndVolumeNeeded)
+
+    return NextResponse.json({ success: true, calculation: tubesAndVolumeNeeded, text: tubesNeeded })
   } catch (error) {
     console.error('Error processing with OpenAI Vision:', error)
     return NextResponse.json({ success: false, message: 'Processing failed' })
@@ -182,24 +395,56 @@ async function pdfToText(image: Buffer) {
     throw error
   }
 }
-async function textToTubes(extractedText: string) {
+async function textToTubeTypeAndVolume(extractedText: string) {
   const prompt = `
-  Extract the following information from this text and ${tests}:
+    Given the following text that includes tests ordered and their corresponding CPT codes:
 
-  1. List all the tests ordered on each lab request form.
-  2. Given the tests, look up the tube and volume that is required.
-  3. Based on the required tubes and volumes, calculate the total number of tubes needed and group them by tube type.
- 
+    1. Identify all test names and CPT codes from the text.
+    2. For each test, match it with its corresponding tube type and volume using the provided reference list.
+    3. Calculate the total volume required for each tube type.
+    4. Determine the number of tubes required for each tube type by considering the total available volume of each tube from the provided totalTubeVolume list. If the total volume for a tube type exceeds its capacity, add another tube.
 
+    Output the result in this format:
 
- Your only output should be this information and nothing else. Be super laconic. 
-  Total Tubes Required by Type:
-  -type of tube: [] tubes
+    Total Tubes Required by Type:
+    - [tubeType]: [number of tubes] tubes (Total Volume: [total volume in mL])
 
+    Example:
+    - gel-barrier: 2 tubes (Total Volume: 9 mL)
+    - lavender-top: 1 tube (Total Volume: 4 mL)
 
-  Replace "X" with the number of tubes for each tube type.
-  
-  Here is the text: 
+    Here is the reference list for the tests:
+    ${JSON.stringify(testsReference)}
+
+    Here is the reference list for totalTubeVolume:
+    ${JSON.stringify(totalTubeVolume)}
+
+    And here is the extracted text:
+    ${extractedText}
+  `
+
+  try {
+    const response = await openAi.chat.completions.create({
+      model: 'gpt-4o-mini',
+      temperature: 0,
+      messages: [
+        {
+          role: 'user',
+          content: prompt
+        }
+      ]
+    })
+    return response.choices[0].message.content
+  } catch (error) {
+    console.error('Error processing with OpenAI Vision:', error)
+    throw error
+  }
+}
+
+async function totalTubesNeeded(extractedText: string | null) {
+  const prompt = `
+   Output Total Tubes by type and nothing else.
+   
   `
   try {
     const response = await openAi.chat.completions.create({
@@ -222,11 +467,11 @@ async function textToTubes(extractedText: string) {
 // Function to convert PDF to images (using pdf2pic)
 async function convertPdfToImages(pdfBuffer: Buffer) {
   const options = {
-    density: 150, // Increase density for higher image quality
-    format: 'jpeg', // Output format
-    width: 1024, // Increase width for better detail
-    height: 1024, // Increase height for better detail
-    quality: 90 // Increase image quality to 90%
+    density: 300, // Increase density for higher image quality
+    format: 'jpeg',
+    width: 1024,
+    height: 1024,
+    quality: 100
   }
 
   const converter = fromBuffer(pdfBuffer, options)
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@
 
 import { useState } from 'react'
 import axios from 'axios'
+import { PhotoIcon, ArrowPathIcon } from '@heroicons/react/24/solid'
 
 export default function FileUploadForm() {
   const [file, setFile] = useState<File | null>(null)
+  const [calculation, setCalculation] = useState('')
   const [response, setResponse] = useState('')
+  const [processing, setProcessing] = useState(false)
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
@@ -25,6 +28,7 @@ export default function FileUploadForm() {
     formData.append('file', file)
 
     try {
+      setProcessing(true)
       // Use axios to send the formData to the API route
       const response = await axios.post('/api/req-pdf', formData, {
         headers: {
@@ -33,19 +37,57 @@ export default function FileUploadForm() {
       })
 
       console.log('File uploaded successfully:', response.data)
+      setProcessing(false)
+
       setResponse(response.data.text)
+      setCalculation(response.data.calculation)
     } catch (error) {
       console.error('Error uploading file:', error)
     }
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type='file' onChange={handleFileChange} accept='application/pdf' />
-        <button type='submit'>Upload PDF</button>
+    <div className='m-auto mt-64   '>
+      <form className='flex flex-col' onSubmit={handleSubmit}>
+        <div className='col-span-full'>
+          <label htmlFor='cover-photo' className='block text-sm font-medium leading-6 text-gray-900'>
+            Requisition from
+          </label>
+          <div className='mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10'>
+            <div className='text-center'>
+              <PhotoIcon aria-hidden='true' className='mx-auto h-12 w-12 text-gray-300' />
+              <div className='mt-4 flex text-sm leading-6 text-gray-600'>
+                <label
+                  htmlFor='file-upload'
+                  className='relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500'>
+                  <span>Upload a requisition form </span>
+                  <input
+                    onChange={handleFileChange}
+                    accept='application/pdf'
+                    id='file-upload'
+                    name='file-upload'
+                    type='file'
+                    className='sr-only'
+                  />
+                </label>
+                <p className='pl-1'>or drag and drop</p>
+              </div>
+              <p className='text-xs leading-5 text-gray-600'>PDF</p>
+            </div>
+          </div>
+        </div>
+        <button
+          type='submit'
+          className=' flex space-x-2 justify-center items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+          <span>{!processing ? 'Upload PDF requisition' : 'Processing'}</span>
+          {processing && <ArrowPathIcon className='w-6 animate-spin' />}
+        </button>
       </form>
-      {response !== '' && <p>{response}</p>}
+      {response && calculation !== '' && (
+        <div>
+          <p>{calculation}</p> <p className='text-red-300 text-lg'>{response}</p>{' '}
+        </div>
+      )}
     </div>
   )
 }
